192066a583f4cdb881a913ad1e969c57fd302866
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 00:46:33 -0500

    feat: Add icon components for navigation and display

---
+++ src/app/components/atoms/Icons/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
+
+const NavIcon = ({ icon }: { icon: string }) => {
+    return (
+        <div className="flex justify-center items-center">
+            <Icon icon={icon} width="37" height="37" className="text-white"/>
+        </div>
+    );
+}
+
+
+const EyeIcon = ({ icon }: { icon: string }) => {
+  return <Icon icon={icon} width="20" height="20" className="text-black" />;
+};
+
+
+const GreaterIcon = () => {
+    return (
+        <Image
+        src="/Container.png"
+        alt="Check mark"
+        width={15}
+        height={15}
+      />
+    );
+}
+
+const PersonIcon= ({ icon }: { icon: string }) => {
+    return (
+        <div className="flex justify-center items-center text-amber-400">
+            <Icon icon={icon} width="24" height="24" className="text-black"/>
+        </div>
+    );
+}
+
+export { NavIcon,EyeIcon,GreaterIcon, PersonIcon};
