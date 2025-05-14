db587f2cece8a48edf658a641941b7a23cd01a3e
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 03:03:25 -0500

    feat: Add additional text title components for varied styling options

--- src/app/components/atoms/Titles/index.tsx
+++ src/app/components/atoms/Titles/index.tsx
@@ -15,7 +15,13 @@ const TextTitle = ({text}: {text: string}) => {
         </h1>
     );
 };
-
+const TextTitle2 = ({text}: {text: string}) => {
+    return (
+        <h1 className='text-[24px] font-semibold'>
+           {text}
+        </h1>
+    );
+};
 const TextRegular = ({text}: {text: string}) => {
     return (
         <h1 className='text-[14px] font-normal'>
@@ -30,5 +36,34 @@ const TextRegular2 = ({text}: {text: string}) => {
         </h1>
     );
 };
+const TextRegular3 = ({text}: {text: string}) => {
+    return (
+        <h1 className='text-[19px] font-semibold text-[#7C3785] text-center mx-4 '>
+           {text}
+        </h1>
+    );
+};
+const TextRegular4 = ({text}: {text: string}) => {
+    return (
+        <h1 className='text-[19px] font-semibold  '>
+           {text}
+        </h1>
+    );
+};
+const TextRegular5 = ({text}: {text: string}) => {
+    return (
+        <h1 className='text-[19px] font-semibold  text-[#71717A]  '>
+           {text}
+        </h1>
+    );
+};
+const TextRegular6 = ({ text }: { text: string }) => {
+  return (
+    <h1 className="relative inline-block text-[15px] font-semibold text-[#71717A]">
+      {text}
+      <span className="absolute top-1/2 left-0 w-full border-t-2 border-[#71717A] transform -translate-y-1/2"></span>
+    </h1>
+  );
+};
 
-export  {TextTitleName, TextTitle, TextRegular,TextRegular2}
+export  {TextTitleName, TextTitle,TextTitle2, TextRegular,TextRegular2,TextRegular3,TextRegular4,TextRegular5,TextRegular6}
