ab0568380bea2c4c267417fc09b5a837d1a2df25
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 03:04:35 -0500

    feat: Add promotions page with MainLayout and Promotions component

---
+++ src/pages/promotions/index.tsx
@@ -0,0 +1,10 @@
+
+import Promotions from '@/app/components/organisms/Promotions';
+import MainLayout from '../MainLayout';
+
+export default function Page() {
+  return <MainLayout>
+     <Promotions></Promotions>
+  </MainLayout>;
+}
+
