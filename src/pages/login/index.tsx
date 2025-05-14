624b85484c2ca648247136860035191df21fccc8
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 00:48:22 -0500

    feat: Add Login page with LoginForm component and MainLayout integration

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/components/organisms/LoginForm';
+import MainLayout from '../MainLayout';
+
+export default function Page() {
+  return <MainLayout>
+      <LoginForm></LoginForm>
+  </MainLayout>;
+}
+
