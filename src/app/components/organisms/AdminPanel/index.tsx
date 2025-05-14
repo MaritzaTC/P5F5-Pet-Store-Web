e9c889550fe13b26052a63e1ac446559b7641ded
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 03:04:13 -0500

    feat: Create AdminPanel component for managing promotions

---
+++ src/app/components/organisms/AdminPanel/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { TextRegular, TextRegular2, TextTitle2 } from '../../atoms/Titles'
+import { GreaterIcon } from '../../atoms/Icons'
+import Card from '../../molecules/Card'
+
+export default function Index() {
+    return (
+        <div>
+            <div className='flex gap-4 ml-20 px-2 mt-6'>
+                <TextRegular2 text='Inicio'></TextRegular2>
+                <GreaterIcon></GreaterIcon>
+                <TextRegular2 text='Panel de Administardor'></TextRegular2>
+                <GreaterIcon></GreaterIcon>
+                <TextRegular text='Promociones'></TextRegular>
+            </div>
+            <div className='flex flex-col items-center justify-center mt-10'>
+                <TextTitle2 text='Gestión de Promociones'></TextTitle2>
+                <div className='grid h-56 grid-rows-3 content-start gap-4 ...'>
+                    <Card></Card>
+                </div>
+
+            </div>
+        </div>
+    )
+}
