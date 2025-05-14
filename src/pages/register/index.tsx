8ba2403bc830cd83201b11e02d4a1c00f2fd2d62
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 00:48:17 -0500

    feat: Create Register page with FormRegister component and MainLayout integration

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,9 @@
+import FormRegister from '@/app/components/organisms/RegisterForm/index';
+import MainLayout from '../MainLayout';
+
+export default function Page() {
+  return <MainLayout>
+      <FormRegister></FormRegister>
+  </MainLayout>;
+}
+
