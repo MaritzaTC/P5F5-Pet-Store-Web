95a8f61abe7bac7824a66623660da6b5240a2154
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 03:03:13 -0500

    feat: Add ButtonRounded2 component with rounded-full styling

--- src/app/components/atoms/Buttons/index.tsx
+++ src/app/components/atoms/Buttons/index.tsx
@@ -8,4 +8,12 @@ const ButtonRounded = ({ text, className }: { text: string; className?: string }
   );
 };
 
-export { ButtonRounded };
+const ButtonRounded2 = ({ text, className }: { text: string; className?: string }) => {
+  return (
+    <button className={`rounded-full font-bold text-[12px] bg-white w-[59px] h-[22px] text-[#7C3785] border-[#7C3785] border-1 ${className ?? ''}`}>
+      {text}
+    </button>
+  );
+}
+
+export { ButtonRounded,ButtonRounded2};
