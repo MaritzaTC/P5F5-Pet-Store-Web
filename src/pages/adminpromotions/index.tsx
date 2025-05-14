a9714f552c6e25fdc582e645518c7e8c6019e59b
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 03:04:31 -0500

    feat: Create admin promotions page with AdminPanel component

---
+++ src/pages/adminpromotions/index.tsx
@@ -0,0 +1,10 @@
+
+import AdminPanel from '@/app/components/organisms/AdminPanel';
+import MainLayout from '../MainLayout';
+
+export default function Page() {
+  return <MainLayout>
+      <AdminPanel></AdminPanel>
+  </MainLayout>;
+}
+
