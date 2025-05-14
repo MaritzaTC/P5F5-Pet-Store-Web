0d5efcfa299e8a66d4525d17ad2f5c631ca244e6
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 14:57:08 -0500

    fix: Adjust Navbar layout for better responsiveness and spacing

--- src/app/components/molecules/Navbar/index.tsx
+++ src/app/components/molecules/Navbar/index.tsx
@@ -4,19 +4,17 @@ import { NavIcon } from '@/app/components/atoms/Icons/index'
 
 const Index = () => {
     return (
-        <div className="bg-[#7C3785] w-full h-[101px] flex items-center justify-between px-8">
-            <div className='flex items-center gap-10'>
+        <div className="bg-[#7C3785] w-screen h-[101px] flex items-center justify-between md:px-8">
+            <div className='flex items-center ml-4 sm:ml-10 md:ml-20'>
                 <TextTitleName text="PetStore" />
                 <NavIcon icon='tabler:baseline-density-medium'></NavIcon>
             </div>
-            <div className="flex gap-10 mr-20">
+            <div className="flex gap-0 sm:gap-0 md:gap-10 mr-1 sm:mr-10 md:mr-20">
                 <NavIcon icon='tabler:logout'></NavIcon>
                 <NavIcon icon='tabler:shopping-cart'></NavIcon>
             </div>
-
         </div>
     )
-
 }
 
 export default Index
