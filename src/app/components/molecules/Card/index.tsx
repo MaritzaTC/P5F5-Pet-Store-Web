d7781974938680b12a40d758aec82b311d2611a0
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 14:57:04 -0500

    fix: Update icon paths in Card component to use correct image URLs

--- src/app/components/molecules/Card/index.tsx
+++ src/app/components/molecules/Card/index.tsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import { TextRegular3 } from '../../atoms/Titles'
-import { CardIcon } from '../../atoms/Icons'
+import {  CardIconImg } from '../../atoms/Icons'
 
 const cardData = [
     {
         title: 'Crear Promoción Simple',
-        icon: 'tabler:calendar-plus',
+        icon: '/tabler-icon-calendar-plus.png',
     },
     {
         title: 'Crear Promoción Por Categoria',
-        icon: 'tabler:calendar-plus',
+        icon: '/tabler-icon-calendar-plus.png',
     },
     {
         title: 'Eliminar Promoción',
-        icon: 'tabler:calendar-minus',
+        icon: '/tabler-icon-calendar-minus.png',
     },
     {
         title: 'Estado de Promociones',
-        icon: 'tabler:calendar-off',
+        icon: '/tabler-icon-calendar-off.png',
     },
     {
         title: 'Lista de Promociones',
-        icon: 'tabler:list-details',
+        icon: '/tabler-icon-list-details.png',
     },
     {
         title: 'Lista de Borradores',
-        icon: 'tabler:eraser',
+        icon: '/tabler-icon-eraser.png',
     }
 ]
 
@@ -36,7 +36,7 @@ export default function Index() {
         <div key={item.title} className='flex flex-col items-center justify-center bg-[#FCFCFC] w-[198px] h-[213px] rounded-[8px] border-[#7C3785] border-2 shadow-md my-2 mx-4 '>
                 <TextRegular3 text={item.title}></TextRegular3>
                    <hr className="w-[100px] h-[0.5px] my-3 border-t border-[#7C3785]" />
-                <CardIcon icon={item.icon}></CardIcon>
+                <CardIconImg url={item.icon}></CardIconImg>
         </div>
         ))}
     </div>
