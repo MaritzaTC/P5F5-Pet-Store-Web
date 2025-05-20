c36108eba884dc0c78c868cd6cd4daea44e22409
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-20 01:13:13 -0500

    feat: Implement Search component for user input functionality

---
+++ src/app/components/molecules/Search/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div> 
+<form className="max-w-md mx-auto">   
+    <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        </div>
+        <input type="search" id="default-search" className="block w-[811px] p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" placeholder="Buscar" required />
+    </div>
+</form>
+        </div>
+  )
+}
