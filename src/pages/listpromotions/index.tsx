084b0f0f484dfa7c5794eca4a8d23420638fde4f
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-20 00:40:34 -0500

    feat: Create index page for ListPromotions component

---
+++ src/pages/listpromotions/index.tsx
@@ -0,0 +1,11 @@
+import ListPromotions from '@/app/components/organisms/ListPromotions';
+import React from 'react'
+import MainLayout from '../MainLayout';
+
+export default function index() {
+  return (
+   <MainLayout>
+      <ListPromotions></ListPromotions>
+  </MainLayout>
+  )
+}
