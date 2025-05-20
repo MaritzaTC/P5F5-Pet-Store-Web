3817aeac56e33be98fb84cbdb5bed998715d6f0f
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-20 01:13:19 -0500

    feat: Add Search and Dropdown components to ListPromotions for enhanced filtering options

--- src/app/components/organisms/ListPromotions/index.tsx
+++ src/app/components/organisms/ListPromotions/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextRegular, TextRegular2, TextTitle } from '../../atoms/Titles'
 import { GreaterIcon } from '../../atoms/Icons'
 import Table from '../../molecules/Table'
+import Search from '../../molecules/Search'
+import { Dropdown } from '../../molecules/Dropdowm'
+import { ButtonCard2 } from '../../atoms/Buttons'
 
 export default function index() {
+     const [selectedOption, setSelectedOption] = useState('');
   return (
     <div>
         <div className='flex gap-4 ml-2 sm:ml-10 md:ml-20 px-2 mt-6'>
@@ -17,7 +21,31 @@ export default function index() {
             </div>  
             <div className='flex flex-col gap-4 ml-2 sm:ml-10 md:ml-20 px-2 mt-6'>
                     <TextTitle text='Lista de Promociones'></TextTitle>
+                    <div className='flex items-center justify-between w-[1201px] ml-4'>
+                        <Search></Search>
+                    <Dropdown
+  className="w-[266px]"
+  label=""
+  name="categoria"
+  value={selectedOption}
+  onChange={(e) => setSelectedOption(e.target.value)}
+  options={[
+    'Alimentos y nutrición',
+    'Higiene y cuidado',
+    'Juguetes y entretenimiento',
+    'Accesorios',
+    'Salud y bienestar',
+    'Hogar y control de olores'
+  ]}
+  placeholder="Categoría" // ← aquí defines el texto
+/>
+                    </div>
                        <Table></Table>
+                       <div className='flex items-center justify-end w-[1201px] ml-4 gap-10'>
+  <ButtonCard2 text={'Anterior'}></ButtonCard2>
+                         <ButtonCard2 text={'Siguiente'}></ButtonCard2>
+                       </div>
+                     
             </div>
 
     </div>
