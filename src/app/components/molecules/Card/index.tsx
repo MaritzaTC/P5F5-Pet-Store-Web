f3a2c01547ca89b9655c31ab086b768f4b013187
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-19 23:55:40 -0500

    refactor: Simplify card rendering logic and enhance link handling in Card component

--- src/app/components/molecules/Card/index.tsx
+++ src/app/components/molecules/Card/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { TextRegular3 } from '../../atoms/Titles'
 import {  CardIconImg } from '../../atoms/Icons'
+import Link from 'next/link'
 
 const cardData = [
     {
@@ -38,15 +39,26 @@ const cardData = [
 export default function Index() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-        {cardData.map((item)=> (
-        <div key={item.title} className='flex flex-col items-center justify-center bg-[#FCFCFC] w-[198px] h-[213px] rounded-[8px] border-[#7C3785] border-2 shadow-md my-2 mx-4 '>
-                <TextRegular3 text={item.title}></TextRegular3>
-                   <hr className="w-[100px] h-[0.5px] my-3 border-t border-[#7C3785]" />
-                <CardIconImg url={item.icon}></CardIconImg>
-        </div>
+      {cardData.map((item) => {
+        const card = (
+          <div
+            key={item.title}
+            className="flex flex-col items-center justify-center bg-[#FCFCFC] w-[198px] h-[213px] rounded-[8px] border-[#7C3785] border-2 shadow-md my-2 mx-4 cursor-pointer hover:shadow-lg transition"
+          >
+            <TextRegular3 text={item.title} />
+            <hr className="w-[100px] h-[0.5px] my-3 border-t border-[#7C3785]" />
+            <CardIconImg url={item.icon} />
+          </div>
         )
-        
-        )}
+
+        return item.href ? (
+          <Link href={item.href} key={item.title}>
+            {card}
+          </Link>
+        ) : (
+          card
+        )
+      })}
     </div>
   )
 }
