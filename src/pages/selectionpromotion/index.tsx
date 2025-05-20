dff6c41be4c5a51b5c77ca72d3be8b1a98ceef46
xMichaelz <michael.molina@udea.edu.co>
2025-05-19 23:50:34 -0500

    Cambios en HU7

---
+++ src/pages/selectionpromotion/index.tsx
@@ -0,0 +1,8 @@
+import MainLayout from '../MainLayout';
+import PromotionManagement from '@/app/components/organisms/PromotionManagement';
+
+export default function Page() {
+    return <MainLayout>
+        <PromotionManagement></PromotionManagement>
+    </MainLayout>
+}
