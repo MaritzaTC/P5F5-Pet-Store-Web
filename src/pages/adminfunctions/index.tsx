dbe1786f8d7bb9672da9340d7eb96e205a5e5e22
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-19 23:55:58 -0500

    feat: Add index page for AdminFunctions component

---
+++ src/pages/adminfunctions/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainLayout from '../MainLayout';
+import AdminFunctions from '@/app/components/organisms/AdminFunctions';
+
+export default function index() {
+  return (
+    <MainLayout>
+      <AdminFunctions></AdminFunctions>
+  </MainLayout>
+  )
+}
