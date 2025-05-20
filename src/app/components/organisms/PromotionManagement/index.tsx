e7c69c6e0f652b201a7a4c8365181c90fdcf6ceb
xMichaelz <michael.molina@udea.edu.co>
2025-05-19 23:53:37 -0500

    cambios en hu7

--- src/app/components/organisms/PromotionManagement/index.tsx
+++ src/app/components/organisms/PromotionManagement/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextRegular, TextRegular2, TextTitle, TextTitle2 } from '../../atoms/Titles'
+import { TextRegular, TextRegular2, TextTitle2 } from '../../atoms/Titles'
 import { GreaterIcon } from '../../atoms/Icons';
 import { CreateBox } from '../../molecules/SelectionBoxes';
 
