80b2d6e0373bf6590486b461a0a29d756e89c0b7
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-19 23:24:34 -0500

    fix: Update input border color for consistent styling in CreateInput component

--- src/app/components/molecules/MiniCard/index.tsx
+++ src/app/components/molecules/MiniCard/index.tsx
@@ -3,13 +3,15 @@ import { TextRegular7} from '../../atoms/Titles'
 
 
 
+
 const CreateInput = ({text}: {text: string}) => {
+       
     return (
         <div className='flex items-center gap-4 w-full max-w-md'>
             <TextRegular7 text={text}></TextRegular7>
             <input
             type='text'
-            className='border border-[#767474] rounded w-[266px] h-[40px] px-2'
+            className='border border-[#E4E4E7] rounded w-[266px] h-[40px] px-2'
             />
         </div>  
     );
