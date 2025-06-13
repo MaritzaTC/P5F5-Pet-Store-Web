c8523ea17ec1d4e5dd50d24417af3529f2494a4f
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-13 15:56:55 -0500

    fix: Enhance ButtonRounded to accept additional button attributes

--- src/app/components/atoms/Buttons/index.tsx
+++ src/app/components/atoms/Buttons/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ButtonRounded = ({ text, className }: { text: string; className?: string }) => {
+const ButtonRounded = ({ text, className,  ...rest}: { text: string; className?: string} & React.ButtonHTMLAttributes<HTMLButtonElement> ) => {
   return (
-    <button  className={`rounded-[6px]  px-4 py-2 ${className ?? ''}`}>
+    <button  className={`rounded-[6px]  px-4 py-2 ${className ?? ''}`} {...rest}>
       {text}
     </button>
   );
 };
 
-const ButtonRounded2 = ({ text, className }: { text: string; className?: string }) => {
+const ButtonRounded2 = ({ text, className}: { text: string; className?: string}) => {
   return (
     <button className={`rounded-full font-bold text-[12px] bg-white w-[59px] h-[22px] text-[#7C3785] border-[#7C3785] border-1 ${className ?? ''}`}>
-      {text}
+      {text} 
     </button>
   );
 }
