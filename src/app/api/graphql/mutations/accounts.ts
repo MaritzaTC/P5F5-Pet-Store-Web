3c6bbaeceb822ff5397396e56b4e4d2ef13d762c
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-13 15:56:50 -0500

    fix: Change INSERTAR_USUARIO to a const declaration and ensure proper export

--- src/app/api/graphql/mutations/accounts.ts
+++ src/app/api/graphql/mutations/accounts.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const INSERTAR_USUARIO = gql`
+const INSERTAR_USUARIO = gql`
   mutation InsertarUsuario($input: UsuarioInput!) {
     insertarUsuario(input: $input) {
       estado
@@ -10,3 +10,11 @@ export const INSERTAR_USUARIO = gql`
   }
 `;
 
+const USER_LOGIN = gql`
+  mutation loginUsuario($nombreUsuario: String!, $contrasena: String!) {
+    loginUsuario(nombreUsuario: $nombreUsuario, contrasena: $contrasena)
+  }
+`;
+
+
+export { INSERTAR_USUARIO, USER_LOGIN};
