35861fd111c9ca7f3bd6f5eb6f81c5607541e934
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-13 15:57:00 -0500

    feat: Implement user authentication state and logout functionality in Navbar

--- src/app/components/molecules/Navbar/index.tsx
+++ src/app/components/molecules/Navbar/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TextTitleName } from '@/app/components/atoms/Titles/index'
 import { NavIcon } from '@/app/components/atoms/Icons/index'
+import { useRouter } from 'next/router';
 
 const Index = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const router = useRouter();
+
+    // Detectar token al cargar
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const token = localStorage.getItem('token');
+            setIsLoggedIn(!!token);
+        }
+    }, []);
+
+    // Función para cerrar sesión
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+        router.push('/login');
+    };
     return (
         <div className="bg-[#7C3785] w-screen h-full flex items-center justify-between gap-10 md:px-8 ">
             <div className='flex items-center ml-4 gap-10 sm:ml-10 md:ml-20'>
@@ -10,7 +28,11 @@ const Index = () => {
                 <NavIcon icon='tabler:baseline-density-medium'></NavIcon>
             </div>
             <div className="flex gap-0 sm:gap-0 md:gap-10 mr-1 sm:mr-10 md:mr-20">
-                <NavIcon icon='tabler:logout'></NavIcon>
+                {isLoggedIn && (
+                    <button onClick={handleLogout}>
+                        <NavIcon icon='tabler:logout' />
+                    </button>
+                )}
                 <NavIcon icon='tabler:shopping-cart'></NavIcon>
             </div>
         </div>
