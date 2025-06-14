98c2b6548bea9254d16ab9a390960ea68415b8dc
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-14 18:32:42 -0500

    refactor: remove unused fields from CrearPromocion mutation

--- src/app/api/graphql/mutations/promotions.ts
+++ src/app/api/graphql/mutations/promotions.ts
@@ -3,14 +3,11 @@ import { gql } from '@apollo/client';
 const CREAR_PROMOCION = gql`
   mutation CrearPromocion($input: PromocionInput!) {
     crearPromocion(input: $input) {
-      promocionId
-      titulo
       activa
-      fechaInicio
-      fechaFin
-      porcentajeDescuento
     }
   }
 `;
+
+
 
 export { CREAR_PROMOCION };
