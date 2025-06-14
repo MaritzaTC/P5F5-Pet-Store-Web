67a1a71f1dfb3596f7a408e6d461546c905629c7
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-14 18:32:15 -0500

    feat: update Dropdown component to use DropdownOption interface for options

--- src/app/components/molecules/Dropdowm/index.tsx
+++ src/app/components/molecules/Dropdowm/index.tsx
@@ -3,13 +3,17 @@ import { TextRegular } from '@/app/components/atoms/Titles/index';
 interface DropdownProps {
     label: string;
     name: string;
-    options: string[];
+   options: DropdownOption[];
     value: string;
     onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
     className?: string;
      placeholder?: string; 
 }
 
+interface DropdownOption {
+  value: string;
+  label: string;
+}
 export const Dropdown: React.FC<DropdownProps> = ({ label, name, options, value, onChange, className = '', placeholder }) => {
     return (
         <div className={`flex flex-col ${className}`}>
@@ -22,11 +26,11 @@ export const Dropdown: React.FC<DropdownProps> = ({ label, name, options, value,
                 required
                 className="border-1 border-[#E4E4E7] rounded-md p-2  focus:border-[#E4E4E7]  outline-none"
             >
-              <option value="">{placeholder || 'Seleccionar'}</option>
-                {options.map((opt) => (
-                    <option key={opt} value={opt}>
-                        {opt}
-                    </option>
+               <option value="">{placeholder || 'Seleccionar'}</option>
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
                 ))}
             </select>
         </div>
