4c6dbbe5269b73f5b05dd05d7b877d069dcf2bd2
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-14 23:46:11 -0500

    feat: add token verification to redirect unauthenticated users to login

--- src/app/components/organisms/AdminFunctions/index.tsx
+++ src/app/components/organisms/AdminFunctions/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect } from 'react'
 import { CreateInput2 } from '../../molecules/MiniCard'
 import { TextRegular, TextRegular2, TextTitle2 } from '../../atoms/Titles'
 import { GreaterIcon } from '../../atoms/Icons'
 import Link from 'next/dist/client/link'
+import { useRouter } from 'next/router'
 
 
 export default function index() {
+     const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/login'); // Redirige si no hay token
+    }
+  }, [router]);
     return (
         <div >
               <div className='flex gap-4 ml-20 px-2 mt-6'>
@@ -17,17 +27,23 @@ export default function index() {
 <div className='flex items-center gap-4 w-full max-w-md my-4'>
                 <TextTitle2 text='Funciones de Aministrador'></TextTitle2>
             </div>
-
-            <CreateInput2 text={'Ventas'} icon='tabler:zoom-money'></CreateInput2>
+<Link href='/next'>
+   <CreateInput2 text={'Ventas'} icon='tabler:zoom-money'></CreateInput2>
+</Link>
+         
             <Link href='/adminpromotions'>
                 <CreateInput2 text={'Promociones'} icon='tabler:discount-2'></CreateInput2>
             </Link>
-            <CreateInput2 text={'Inventario'} icon='tabler:clipboard-list'></CreateInput2>
+            <Link href='next'>
+             <CreateInput2 text={'Inventario'} icon='tabler:clipboard-list'></CreateInput2>
+            </Link>
             <Link href='/register'>
             <CreateInput2 text={'Usuarios'} icon='tabler:users-group'></CreateInput2>
             </Link>
-            
-            <CreateInput2 text={'Extra'} icon='tabler:zoom-money'></CreateInput2>
+            <Link href='/register'>
+             <CreateInput2 text={'Crear Usuario'} icon='tabler:user-plus'></CreateInput2>
+            </Link>
+           
             </div>
             
         </div>
