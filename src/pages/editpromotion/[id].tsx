68cb218720c38b23ec215a6f0c6eb6590f410b15
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-14 23:53:23 -0500

    feat: add edit promotion page with MainLayout and EditPromotion component

---
+++ src/pages/editpromotion/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainLayout from '../MainLayout'
+import EditPromotion from '@/app/components/organisms/EditPromotion'
+
+export default function index() {
+  return (
+     <MainLayout>
+      <EditPromotion/>
+    </MainLayout>
+  )
+}
