7fed346b2c3e082b559b8124ec20b3664f647d1d
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-14 23:45:51 -0500

    feat: refactor Search component to accept props for value and onChange

--- src/app/components/molecules/Search/index.tsx
+++ src/app/components/molecules/Search/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
-export default function index() {
+interface SearchProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function index({ value, onChange }: SearchProps) {
   return (
-    <div> 
-<form className="max-w-md mx-auto">   
-    <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-        </div>
-        <input type="search" id="default-search" className="block w-[811px] p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" placeholder="Buscar" required />
-    </div>
-</form>
-        </div>
+      <form className="w-full">
+      <div className="relative w-full">
+        <input
+          type="search"
+          id="default-search"
+          value={value}
+          onChange={onChange}
+          className="w-full p-2 text-sm text-gray-900 border rounded-lg h-[40px]"
+          placeholder="Buscar por nombre de promoción"
+        />
+      </div>
+    </form>
   )
 }
